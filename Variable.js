36891678ed079bdfcf70f894eca249bd2b8adee3
Bae Junho <58718078+Juno-hub@users.noreply.github.com>
2020-08-10 17:53:57 +0900

    Add files via upload

--- Variable.js
+++ Variable.js
@@ -2,7 +2,4 @@ var a = 1;
 console.log(a);
 a = 2;
 console.log(a);
-//1 = 2; comment
-var name = 'egoing';
-var letter = 'Dear '+name+' Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temporincididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud '+name+' exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. '+name+'';
-console.log(letter);
+//1 = 2; comment = 주석
