f1512252fd81a45ae042bfe18c6e74fb57cde5dc
Bae Junho <58718078+Juno-hub@users.noreply.github.com>
2020-08-10 15:56:31 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -2,13 +2,14 @@ var http = require('http');
 var fs = require('fs');
 var app = http.createServer(function(request,response){
     var url = request.url;
-    if(url == '/'){
+    if(request.url == '/'){
       url = '/index.html';
     }
-    if(url == '/favicon.ico'){
+    if(request.url == '/favicon.ico'){
       response.writeHead(404);
       response.end();
       return;
+
     }
     response.writeHead(200);
     response.end(fs.readFileSync(__dirname + url));
