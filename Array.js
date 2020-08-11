49b49e157371898f2e3f8d264e16231fb45a8394
Bae Junho <58718078+Juno-hub@users.noreply.github.com>
2020-08-11 17:53:32 +0900

    Add files via upload

---
+++ Array.js
@@ -0,0 +1,9 @@
+//CRUD
+var arr = ['A','B','C','D']; //Boolean도 배열 안에 들어갈 수 있음.
+console.log(arr[1]);
+console.log(arr[3]);
+arr[2] = 3;
+console.log(arr);
+console.log(arr.length);
+arr.push('E');
+console.log(arr);
---
+++ Comparison.js
@@ -0,0 +1,6 @@
+console.log(1==1); //true
+console.log(1==2); //false
+console.log(1===1); //true, 이유가 없다면 '==' 말고 '==='이라고 사용할 것
+console.log(1===2); //false
+console.log(1>2); //false
+console.log(1<2); //ture
