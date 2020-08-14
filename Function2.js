54c811246ed854567f46ab16ab59bc190e154c0c
Bae Junho <58718078+Juno-hub@users.noreply.github.com>
2020-08-14 15:44:34 +0900

    Add files via upload

---
+++ Function2.js
@@ -0,0 +1,9 @@
+console.log(Math.round(1.6)); //2
+console.log(Math.round(1.4)); //1
+//round는 반올림하는 함수
+
+function sum(first, second){ // parameter(매개변수)
+  return first+second; // return은 거가서 함수를 종료시키고, 출력시킴
+};
+
+console.log(sum(2,4)); // argument(인자)
